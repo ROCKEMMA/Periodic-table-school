842a1f761a2806b8fe37bfff0bbb1702739ff9a8
jefuentes <jossuerm55@gmail.com>
2024-05-27 10:28:42 -0600

    Base del proyecto

---
+++ main.js
@@ -0,0 +1,8 @@
+let DOM = document.getElementById("root");
+
+DOM.innerHTML = `
+    <header></header>
+    <section id="seccion-tabla"></section>
+    <section id="seccion-calculadora"></section>
+    <footer></footer>
+`;
