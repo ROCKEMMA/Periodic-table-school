6b052b08a9e2d77a62994708aca3de81f216c64f
Nyulian122 <ysgaitan@scl.edu.gt>
2024-05-29 12:58:56 -0600

    primera version del header

--- components/cargarHeader/cargarHeader.js
+++ components/cargarHeader/cargarHeader.js
@@ -1,5 +1,26 @@
 function cargarHeader(){
-    console.log("header")
+    
+    let headerDOM = document.querySelector("#header");
+
+    headerDOM.innerHTML = `
+
+    <header>
+        <div class="container">
+            <div class="imagen">
+            </div>
+            <p>Tabla Periodica 2024</p>
+            <nav>
+                <ul>
+                    <li><a href="#">Repositorio</a></li>
+                    <li><a href="#">Integrantes</a></li>
+                    <li><a href="#">Discord</a></li>
+                    <li><a href="#">Github</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+    
+    `
 }
 
 export { cargarHeader }
