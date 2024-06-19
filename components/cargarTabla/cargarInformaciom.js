9a0202f01f56de3883e14f4f987ee15d0514ce43
JUSTINRR88 <justinbercian1@gmail.com>
2024-06-18 19:38:04 -0600

    creacion de cuadro

--- components/cargarTabla/cargarInformaciom.js
+++ components/cargarTabla/cargarInformaciom.js
@@ -1,5 +1,4 @@
 function cargarInformacion(nombreElemento) {
-    let link = document.createElement("link");
     let divElementoCentral = document.querySelector(".informacion");
     
     // Vaciar el contenido existente
@@ -7,19 +6,24 @@ function cargarInformacion(nombreElemento) {
     
     // Crear el nuevo contenido
     let div_elemento = document.createElement("div");
-    div_elemento.class = "estiloElemento"
+    div_elemento.className = "estiloElemento";
     div_elemento.innerHTML = `
-    <div><strong>Descubridor:</strong> ${nombreElemento.descubridor}</div>
+    <div><strong>Símbolo:</strong>${nombreElemento.simbolo}</div>
     <div><strong>Electrones:</strong> ${nombreElemento.electrones}</div>
     <div><strong>Masa:</strong> ${nombreElemento.masa}</div>
     <div><strong>Nombre:</strong> ${nombreElemento.nombre}</div>
     <div><strong>Serie:</strong> ${nombreElemento.serie}</div>
-    <div><strong>Símbolo:</strong> ${nombreElemento.simbolo}</div>
+    <div><strong>Descubridor:</strong> ${nombreElemento.descubridor}</div>
     `;
     divElementoCentral.appendChild(div_elemento);
-    link.rel = "stylesheet";
-    link.href ="components/cargarTabla/estiloElemento.css";
-    document.head.appendChild(link);
+    
+    // Verificar si el link ya existe
+    if (!document.querySelector("link[href='components/cargarTabla/estiloElemento.css']")) {
+        let link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.href = "components/cargarTabla/estiloElemento.css";
+        document.head.appendChild(link);
+    }
 }
 
 export { cargarInformacion };
